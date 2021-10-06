ce8fd93e0d26cdb613a3debf376cc93cf46bf1a4
LAPTOP-9IS59JQ1\Dasha <daria.niemkevych@gmail.com>
2021-10-06 21:10:12 +0300

    initial commit

---
+++ oop/HW3/HW3_2/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PROMPT = "Pick one:\n" +
+                "1) Create group\n" +
+                "2) Add a good student to a group\n" +
+                "3) Add a bad student to a group\n" +
+                "4) Get group info\n" +
+                "5) Get full group info\n" +
+                "6) Study for all students\n" +
+                "7) Quit";
+
+            Group group = null;
+            while (true)
+            {
+                Console.WriteLine(PROMPT);
+                string option = Console.ReadLine();
+                if (option == "1")
+                {
+                    Console.Write("Group name: ");
+                    string name = Console.ReadLine();
+                    group = new Group(name);
+                    Console.WriteLine("Group created!");
+                }
+                else if (option == "7")
+                {
+                    Console.WriteLine("Bye!");
+                    break;
+                }
+                else
+                {
+                    if (group == null)
+                    {
+                        Console.WriteLine("Create a group first!");
+                        continue;
+                    }
+                    if (option == "2" || option == "3")
+                    {
+                        Console.Write("Student name: ");
+                        string name = Console.ReadLine();
+                        Student student;
+                        if (option == "2")
+                        {
+                            student = new GoodStudent(name);
+                        }
+                        else
+                        {
+                            student = new BadStudent(name);
+                        }
+                        group.AddStudent(student);
+                    }
+                    else if (option == "4")
+                    {
+                        group.GetInfo();
+                    }
+                    else if (option == "5")
+                    {
+                        group.GetFullInfo();
+                    }
+                    else if (option == "6")
+                    {
+                        group.StudyAll();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong option!");
+                    }
+                }
+            }
+        }
+    }
+
+    class Group
+    {
+        private string name;
+        private List<Student> students;
+
+        public Group(string name)
+        {
+            this.name = name;
+            students = new List<Student>();
+        }
+
+        public void AddStudent(Student st)
+        {
+            students.Add(st);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine(name);
+            foreach (Student st in students)
+            {
+                Console.WriteLine(st.getName());
+            }
+        }
+
+        public void GetFullInfo()
+        {
+            Console.WriteLine(name);
+            foreach (Student st in students)
+            {
+                Console.WriteLine(st.getName() + " " + st.getState());
+            }
+        }
+
+        public void StudyAll()
+        {
+            foreach (Student st in students)
+            {
+                st.Study();
+            }
+        }
+    }
+
+    abstract class Student
+    {
+        private string name;
+        protected string state;
+
+        public Student(string name)
+        {
+            this.name = name;
+            state = "";
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getState()
+        {
+            return state;
+        }
+
+        public abstract void Study();
+
+        public void Read()
+        {
+            state += " Read";
+        }
+
+        public void Write()
+        {
+            state += " Write";
+        }
+
+        public void Relax()
+        {
+            state += " Relax";
+        }
+    }
+
+    class GoodStudent : Student
+    {
+        public GoodStudent(string name) : base(name)
+        {
+            state += "good";
+        }
+
+        public override void Study()
+        {
+            Read();
+            Write();
+            Read();
+            Write();
+            Relax();
+        }
+
+    }
+
+    class BadStudent : Student
+    {
+        public BadStudent(string name) : base(name)
+        {
+            state += "bad";
+        }
+
+        public override void Study()
+        {
+            Relax();
+            Relax();
+            Relax();
+            Relax();
+            Relax();
+        }
+
+    }
+}
---
+++ oop/HW3/HW3_3/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace HW3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PROMPT = "Pick one:\n" +
+                "1) Convert usd to uah\n" +
+                "2) Convert eur to uah\n" +
+                "3) Convert uah to usd\n" +
+                "4) Convert uah to eur\n" +
+                "5) Quit";
+
+            Converter converter = new Converter(26.3, 30.5);
+
+            while (true)
+            {
+                Console.WriteLine(PROMPT);
+                string option = Console.ReadLine();
+                if (option == "1")
+                {
+                    Console.Write("usd: ");
+                    double usd = Console.Read();
+                    Console.ReadLine();
+                    Console.Write("uah: ");
+                    Console.WriteLine("{0:F}", converter.UsdToUah(usd));
+                }
+                else if (option == "2")
+                {
+                    Console.Write("eur: ");
+                    double eur = Console.Read();
+                    Console.ReadLine();
+                    Console.Write("uah: ");
+                    Console.WriteLine("{0:F}", converter.EurToUah(eur));
+                }
+                else if (option == "3")
+                {
+                    Console.Write("uah: ");
+                    double uah = Console.Read();
+                    Console.ReadLine();
+                    Console.Write("usd: ");
+                    Console.WriteLine("{0:F}", converter.UahToUsd(uah));
+                }
+                else if (option == "4")
+                {
+                    Console.Write("uah: ");
+                    double uah = Console.Read();
+                    Console.ReadLine();
+                    Console.Write("eur: ");
+                    Console.WriteLine("{0:F}", converter.UahToEur(uah));
+                }
+                else if (option == "5")
+                {
+                    Console.WriteLine("Bye!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong option!");
+                }
+            }
+        }
+    }
+
+    class Converter
+    {
+        private double usd;
+        private double eur;
+
+        public Converter(double usd, double eur)
+        {
+            this.usd = usd;
+            this.eur = eur;
+        }
+
+        public double UahToUsd(double uah)
+        {
+            return uah / usd;
+        }
+
+        public double UahToEur(double uah)
+        {
+            return uah / eur;
+        }
+
+        public double UsdToUah(double usd)
+        {
+            return usd * this.usd;
+        }
+
+        public double EurToUah(double eur)
+        {
+            return eur * this.eur;
+        }
+    }
+}
