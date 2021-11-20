f0e51a86916fbb831d7f0b8f30b1dcb2c28498f3
LAPTOP-9IS59JQ1\Dasha <daria.niemkevych@gmail.com>
2021-11-20 22:46:36 +0200

    add HW7

---
+++ oop/HW7/Solid_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Rectangle
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+    public int GetRectangleArea()
+    {
+        return Width * Height;
+    }
+}
+
+//квадрат наслідується від прямокутника!!! <Принцип підстановки Лісков>
+class Square
+{
+    public virtual int Side { get; set; }
+    public int GetSquareArea()
+    {
+        return Side * Side;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle();
+        rect.Width = 5;
+        rect.Height = 10;
+
+        Square square = new Square();
+        square.Side = 10;
+
+        Console.WriteLine(rect.GetRectangleArea());
+        Console.WriteLine(square.GetSquareArea());
+        //Відповідь 100? Що не так???
+        Console.ReadKey();
+    }
+}
---
+++ oop/HW7/Solid_4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/*Даний інтерфейс поганий тим, що він включає занадто багато методів.
+ А що, якщо наш клас товарів не може мати знижок або промокодом, або для нього немає сенсу встановлювати матеріал з 
+ якого зроблений (наприклад, для книг). Таким чином, щоб не реалізовувати в кожному класі невикористовувані в ньому методи, краще 
+розбити інтерфейс на кілька дрібних і кожним конкретним класом реалізовувати потрібні інтерфейси.
+Перепишіть, розбивши інтерфейс на декілька інтерфейсів, керуючись принципом розділення інтерфейсу. 
+Опишіть класи книжки (розмір та колір не потрібні, але потрібна ціна та знижки) та верхній одяг (колір, розмір, ціна знижка),
+які реалізують притаманні їм інтерфейси.
+<Принцип розділення інтерфейсу>*/
+
+interface IItem
+{
+    void SetPrice(double price);
+}
+
+interface IDiscountable
+{
+    void ApplyDiscount(String discount);
+    void ApplyPromocode(String promocode);
+}
+
+interface ISizable
+{
+    void SetSize(byte size);
+}
+
+interface IColorable
+{
+    void SetColor(byte color);
+}
+
+class Book : IItem, IDiscountable
+{
+    public void SetPrice(double price) { }
+    public void ApplyDiscount(String discount) { }
+    public void ApplyPromocode(String promocode) { }
+}
+
+class Cloth : IItem, IDiscountable, ISizable, IColorable
+{
+    public void SetPrice(double price) { }
+    public void ApplyDiscount(String discount) { }
+    public void ApplyPromocode(String promocode) { }
+    public void SetSize(byte size) { }
+    public void SetColor(byte color) { }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.ReadKey();
+    }
+}
