92bc2d6436fa8bf2b58ae7f4c68ecbe8ea78545b
LAPTOP-9IS59JQ1\Dasha <daria.niemkevych@gmail.com>
2021-12-02 00:35:21 +0200

    add hw6

---
+++ oop/HW6/Decorator/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+namespace Decorator.Examples
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            ChristmasTree tree = new ChristmasTree();
+            Lights decorWithLights = new Lights("yellow");
+            Toy decorWithBall = new Toy("ball");
+            Toy decorWithCandy = new Toy("candy");
+
+            decorWithBall.SetComponent(tree);
+            decorWithLights.SetComponent(decorWithBall);
+            decorWithCandy.SetComponent(decorWithLights);
+
+            decorWithCandy.Operation();
+
+            // Wait for user
+            Console.Read();
+        }
+    }
+
+    class ChristmasTree : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("Christmas tree: ");
+        }
+    }
+
+    class Lights : Decorator
+    {
+        private bool isLightsOn = true;
+        private string color;
+
+        public Lights(string color)
+        {
+            this.color = color;
+        }
+
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();   
+        }
+        void AddedBehavior()
+        {
+            if (isLightsOn)
+            {
+                Console.WriteLine(color + " lights on");
+                isLightsOn = false;
+            }
+            else
+            {
+                Console.WriteLine(color + " lights off");
+                isLightsOn = true;
+            }
+        }
+    }
+
+    class Toy : Decorator
+    {
+        private string addedState;
+
+        public Toy(string type)
+        {
+            addedState = type;
+        }
+
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine(addedState);
+        }
+    }
+
+    class DecoratedChristmasTree : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+        }
+    }
+
+    // "Component"
+    abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    // "Decorator"
+    abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+}
---
+++ oop/HW6/Facade/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Examples
+{
+    // Mainapp test application 
+    class MainApp
+    {
+        public static void Main()
+        {
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+            // Wait for user 
+            Console.Read();
+        }
+    }
+
+
+    // "Subsystem ClassA" 
+    class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine(" SubSystemOne Method");
+        }
+    }
+
+    // Subsystem ClassB" 
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine(" SubSystemTwo Method");
+        }
+    }
+
+
+    // Subsystem ClassC" 
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine(" SubSystemThree Method");
+        }
+    }
+    // Subsystem ClassD" 
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine(" SubSystemFour Method");
+        }
+    }
+    // "Facade" 
+    class Facade
+    {
+        SubSystemOne one;
+        SubSystemTwo two;
+        SubSystemThree three;
+        SubSystemFour four;
+
+        public Facade()
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            two.MethodTwo();
+            three.MethodThree();
+        }
+    }
+}
